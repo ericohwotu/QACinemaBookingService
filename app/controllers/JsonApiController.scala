a482775ef5fa90320417ac8cfd9e4daacd9f44f0
ericohwotu <eric.ohwotu@outlook.com>
2017-07-28 11:39:47 +0000

    Added Session Key Generator

--- app/controllers/JsonApiController.scala
+++ app/controllers/JsonApiController.scala
@@ -6,11 +6,32 @@ import play.api.mvc._
 
 class JsonApiController extends Controller{
 
-  def getAllSeats = Action { implicit request: Request[AnyContent] =>
-    Ok(Json.parse(SeatGenerator.getSeats(request.remoteAddress)))
+  def getAllSeats(key: Option[String]) = Action { implicit request: Request[AnyContent] =>
+
+    jsonApiHelper(key, request) match {
+      case "Unauthorised" => Unauthorized("Sorry you are not authorised")
+      case bookingKey => Ok(Json.parse(SeatGenerator.getSeats(bookingKey)))
+    }
+
+
   }
 
-  def bookSeat(id: Int) = Action { implicit request: Request[AnyContent] =>
-    Ok(Json.parse(SeatGenerator.bookSeats(id, request.remoteAddress)))
+  def bookSeat(id: Int, key: Option[String]) = Action { implicit request: Request[AnyContent] =>
+    jsonApiHelper(key, request) match {
+      case "Unauthorised" => Unauthorized("Sorry you are not authorised")
+      case bookingKey => Ok (Json.parse (SeatGenerator.bookSeats (id, bookingKey) ) )
+    }
+
+  }
+
+  def jsonApiHelper(key: Option[String], request: Request[AnyContent]): String ={
+    request.session.get("sessionKey").getOrElse("") match {
+      case "" => key match {
+        case None => "Unauthorised"
+        case apiKey =>
+          apiKey.get
+      }
+      case sessionKey => sessionKey
+    }
   }
 }
